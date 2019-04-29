6739c5e8e9e76de2eabd48dc92eaeb454ec096bc
M-kos <koc.melnikov.1985@gmail.com>
2019-04-29 19:24:08 +0700

    Настройка Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const path = require('path');
 module.exports = {
     entry: './uikit/src/index.js',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, './uikit/dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './uikit/dist')
     },
     module: {
-      rules: [
-        
-      ]
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     }
-  }
+}
